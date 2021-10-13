3c97736470750c0ae414e44bba8f188332c9e9a5
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-10-13 14:27:02 -0700

    Create controller decorator

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import cookieSession from 'cookie-session';
+import { router as controllerRouter } from './controllers/decorators/controller';
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['blalklkj'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
